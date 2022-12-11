08b7722b4c85273ec853b837a161dc540c9c8931
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2022-12-11 21:11:52 +0200

    Update all_code_model.py

--- all_code_model.py
+++ all_code_model.py
@@ -24,7 +24,7 @@ data = data[['A', 'Qin', 'Ca', 'RH','Species']]
 #data = data.sample(frac=1) # Shuffle the data 
 
 ###### Define features and labels ######
-X = data.drop('A)
+X = data[['Qin', 'Ca', 'RH','Species']]
 # Standardization:
 # X = (X-X.mean())/X.std() #standardization
 Y = data.A
@@ -60,8 +60,8 @@ st.sidebar.header('User Input Parameters')
 
 def user_input_features():
     # Create a list of Species for the selectbox
-    #Species = st.sidebar.select_slider("Species", options = ['Tradescantia', 'Peperomia', 'Spathiphyllum', 'Philodendron','Monalisa', 'Chlorophytum'])
-    Species = st.sidebar.slider('Species', 0, 5, 0)
+    Species = st.sidebar.select_slider("Species", options = ['Tradescantia', 'Peperomia', 'Spathiphyllum', 'Philodendron','Monalisa', 'Chlorophytum'])
+    #Species = st.sidebar.slider('Species', 0, 5, 0)
     Qin = st.sidebar.slider('Light Intensity (PAR)', 0, 1200, 70)
     Ca = st.sidebar.slider('Ambient CO_2 levels (ppm)', 0, 1500,400 )
     RH = st.sidebar.slider('Relative Humidity (%)', 20, 80, 60)
@@ -78,7 +78,7 @@ def user_input_features():
     'Philodendron':4,
     'Chlorophytum':5
     }
-    #features['Species'] = features[features.columns[0]].replace(labels_dict_rev)
+    features['Species'] = features['Species'].replace(labels_dict_rev)
     features = features[['Ca','Qin','RH','Species']]
 
     return features
