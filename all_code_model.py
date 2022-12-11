1158aff2e09c2c8fc6e88b55a2686e2fc37752dc
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2022-12-11 20:31:40 +0200

    Update all_code_model.py

--- all_code_model.py
+++ all_code_model.py
@@ -6,74 +6,40 @@ Created on Sun Dec 11 17:25:48 2022
 """
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import xgboost as xgb
-from xgboost import Booster
-#from xgboost import DMatrix
 from xgboost import XGBRegressor
 import seaborn as sns
 sns.set_theme(style="white",font_scale = 1)
 #import sklearn
-#import PIL
-#from PIL import Image
-#from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LinearRegression
+import PIL
+from PIL import Image
+from sklearn.model_selection import train_test_split
 #from sklearn.model_selection import KFold
 #from sklearn.model_selection import cross_validate
 #from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-#from sklearn.model_selection import GridSearchCV
 
 # read the data
-path = "Assimilation_model_data.xlsx"
+path = r"C:\downloads_py\Streamlit\Clean_data.xlsx"
 data = pd.read_excel(path)
 data = data[['A', 'Qin', 'Ca', 'RH','Species']]
-
-labels_dict = {
-    1:'Tradescantia',
-    '1':'Tradescantia',
-    2:'Peperomia',
-    '2':'Peperomia',
-    3:'Spathiphyllum',
-    '3':"Spathiphyllum",
-    'Sphatophilum':'Spathiphyllum',
-    4:'Philodendron',
-    '4': 'Philodendron',
-    5: 'Monalisa',
-    '5': 'Monalisa',
-    6:'Chlorophytum',
-    '6':'Chlorophytum',
-    'Chlorophitum':'Chlorophytum'
-    }
-data['Species'] = data['Species'].replace(labels_dict)
-
-data = data.sample(frac=1) # Shuffle the data 
+#data = data.sample(frac=1) # Shuffle the data 
 
 ###### Define features and labels ######
-X = data[['Ca','Qin','RH']]
+X = data[['Ca','Qin','RH','Species']]
 # Standardization:
 # X = (X-X.mean())/X.std() #standardization
-labels_dict_rev = {
-    'Tradescantia':0,
-    'Peperomia':1,
-    'Spathiphyllum':2,
-    'Monalisa':3,
-    'Philodendron':4,
-    'Chlorophytum':5
-    }
-X['Species'] = data['Species'].replace(labels_dict_rev)
 Y = data.A
 
 
 ###### Split the data into train and test ######
-#X_train_val, X_test, y_train_val, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-#X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, random_state=0)
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, random_state=0)
 xgb_cv = XGBRegressor(colsample_bytree=1.0, learning_rate=0.01, max_depth=7,
              min_child_weight=3, n_estimators=500, objective='reg:squarederror',
              subsample=1.0)
 
 fit_model = xgb_cv.fit(
-    X,
-    Y,
+    X_train,
+    y_train,
     eval_metric='mae',
     verbose=False)
 
@@ -86,8 +52,8 @@ st.markdown("""
 #### You can use this application to match the type of plant with the highest potential for reducing the levels of PAH in the room.
 #### Alternatively, you can get a quantitative estimate of the amount of CO$_2$ levels that the plants reduce indoor	:full_moon_with_face:)
 """)
-#image = Image.open(r"C:\downloads_py\Streamlit\Images\gw_image.jpg")
-#st.image(image, caption='Green Wall in The M&M VS Lab at the Hebrew University')
+image = Image.open(r"C:\downloads_py\Streamlit\Images\gw_image.jpg")
+st.image(image, caption='Green Wall in The M&M VS Lab at the Hebrew University')
 
 
 st.sidebar.header('User Input Parameters')
