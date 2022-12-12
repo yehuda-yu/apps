c4b7e2b13859d54b6fcc2262b4c7dd63560a1d3c
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2022-12-12 11:05:11 +0200

    Update all_code_model.py

--- all_code_model.py
+++ all_code_model.py
@@ -1,22 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 11 17:25:48 2022
-
-@author: User
-"""
-import numpy as np
 import streamlit as st
 import pandas as pd
-from xgboost import XGBRegressor
 import seaborn as sns
 sns.set_theme(style="white",font_scale = 1)
 #import sklearn
-import PIL
-from PIL import Image
-import pickle
+#import PIL
+#from PIL import Image
+from sklearn.ensemble import RandomForestRegressor
+#from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import KFold
+#from sklearn.model_selection import cross_validate
+#from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-
-'''
 # read the data
 path = "Clean_data.xlsx"
 data = pd.read_excel(path)
@@ -24,31 +18,22 @@ data = data[['A', 'Qin', 'Ca', 'RH','Species']]
 #data = data.sample(frac=1) # Shuffle the data 
 
 ###### Define features and labels ######
-X = data[['Qin', 'Ca', 'RH','Species']]
+X = data[['Ca','Qin','RH','Species']]
 # Standardization:
 # X = (X-X.mean())/X.std() #standardization
 Y = data.A
 
 
 ###### Split the data into train and test ######
-X_train_val, X_test, y_train_val, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, random_state=0)
-eval_set = [(X_train, y_train),
-            (X_val, y_val)]
+#X_train_val, X_test, y_train_val, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+#X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, random_state=0)
+#xgb_cv = XGBRegressor(colsample_bytree=1.0, learning_rate=0.01, max_depth=7,
+#             min_child_weight=3, n_estimators=500, objective='reg:squarederror',
+#             subsample=1.0)
 
-xgb_cv = (colsample_bytree=1.0, learning_rate=0.01, max_depth=7,
-             min_child_weight=3, n_estimators=500, objective='reg:squarederror',
-             subsample=1.0)
+fit_model = RandomForestRegressor(max_depth=100, n_estimators=800).fit(X,Y)
 
-fit_model = xgb_cv.fit(
-    X_train_val,
-    y_train_val,
-  set_params=eval_set,
-  eval_metric='mae',
-  early_stopping_rounds=50,
-  verbose=2
-)
-'''
+
 
 st.markdown("""
 # CO$_2$ Assimilation Prediction app
@@ -61,12 +46,12 @@ st.markdown("""
 #image = Image.open(r"C:\downloads_py\Streamlit\Images\gw_image.jpg")
 #st.image(image, caption='Green Wall in The M&M VS Lab at the Hebrew University')
 
+
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
     # Create a list of Species for the selectbox
     Species = st.sidebar.select_slider("Species", options = ['Tradescantia', 'Peperomia', 'Spathiphyllum', 'Philodendron','Monalisa', 'Chlorophytum'])
-    #Species = st.sidebar.slider('Species', 0, 5, 0)
     Qin = st.sidebar.slider('Light Intensity (PAR)', 0, 1200, 70)
     Ca = st.sidebar.slider('Ambient CO_2 levels (ppm)', 0, 1500,400 )
     RH = st.sidebar.slider('Relative Humidity (%)', 20, 80, 60)
@@ -94,10 +79,7 @@ st.subheader('User Input parameters')
 st.write(df)
 
 # Prediction:
-with open("XGB.pkl", 'rb') as file:
-    model = pickle.load(file)
-
-prediction = model.predict(df)
-#prediction = fit_model.predict(df)
+prediction = fit_model.predict(df)
+import numpy as np
 st.subheader(f'$CO_2$ Assimilation rate prediction {np.round(prediction,2)} µmol m$^2 s^{-1}$')
 st.write(prediction)
